2721ac61114d82a8edbb3b508e7ffee1b7660af5
Tom <chadjiminas.chrysostomos@gmail.com>
2023-08-02 15:50:09 +0300

    add --use-wandb to train_body_vq_gestformer.sh

--- train_body_vq_gestformer.sh
+++ train_body_vq_gestformer.sh
@@ -5,5 +5,6 @@ python scripts/train.py \
 --exp_name smplx_gestformer_encoder \
 --speakers oliver seth conan chemistry \
 --config_file ./config/body_vq_gestform.json \
---device cuda:0
+--device cuda:0 \
+--use-wandb
 
